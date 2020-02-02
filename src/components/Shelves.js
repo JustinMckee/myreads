2a05dc9b0c56efd4ec10cf638245724664774660
Justin McKee <thisisjustinmckee@gmail.com>
2020-02-02 17:39:31 -0500

    Filters books that belong on each shelf

--- src/components/Shelves.js
+++ src/components/Shelves.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from 'react'
+import {Link} from 'react-router-dom'
 import Shelf from './Shelf.js'
 
-class Shelves extends React.Component {
-	render() {
-		return (
-			<div className="list-books">
-	      <div className="list-books-title">
-	        <h1>MyReads</h1>
-	      </div>
-	      <div className="list-books-content">
-	        <Shelf/>
-	        <Shelf/>
-	        <Shelf/>
-	      </div>
+const Shelves = ({books}) => {
 
-	      <div className="open-search">
-	        <Link to='/search'>Add a book</Link>
-	      </div>
-	    </div>
-		)
-	}
+	const filterBooksOnShelf = shelfName => {
+		let booksOnShelf = books.filter(book => {
+			return book.shelf === shelfName
+		})
+		return booksOnShelf
+	};
+
+	return (
+		<div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <Shelf
+					title="Currently Reading"
+					books={filterBooksOnShelf('currentlyReading')}
+				/>
+        <Shelf
+					title="Want to Read"
+					books={filterBooksOnShelf('wantToRead')}
+				/>
+        <Shelf
+					title="Read"
+					books={filterBooksOnShelf('read')}
+				/>
+      </div>
+
+      <div className="open-search">
+        <Link to='/search'>Add a book</Link>
+      </div>
+    </div>
+	);
+
 }
 
 export default Shelves
