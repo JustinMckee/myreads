43d5b561c16b1916e268bbb513a7311cce0c5f3b
Justin McKee <thisisjustinmckee@gmail.com>
2020-02-02 14:24:53 -0500

    Adds basic components

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Search extends React.Component {
+	render() {
+		return (
+				<h2>Search</h2>
+		)
+	}
+}
+
+export default Search
---
+++ src/components/Shelves.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import Shelf from './Shelf.js'
+
+class Shelves extends React.Component {
+	render() {
+		return (
+			<div className="list-books">
+	      <div className="list-books-title">
+	        <h1>MyReads</h1>
+	      </div>
+	      <div className="list-books-content">
+	        <Shelf/>
+	        <Shelf/>
+	        <Shelf/>
+	      </div>
+
+	      <div className="open-search">
+	        <Link to='/search'>Add a book</Link>
+	      </div>
+	    </div>
+		)
+	}
+}
+
+export default Shelves
