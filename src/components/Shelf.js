35e56e42fbf0bc751cbe3e9910fa4c64a9f0338f
Justin McKee <thisisjustinmckee@gmail.com>
2020-02-02 17:49:36 -0500

    Shows all books properties

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,21 +1,33 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Book from './Book.js'
 
-class Shelf extends Component {
-  render(){
-    return(
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-              <Book />
-            </li>
-          </ol>
-        </div>
+const Shelf = ({title,books}) => {
+
+  return(
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+					{books.map(book => (
+						<li key={book.id}>
+							<Book
+								thumb={
+									book.imageLinks
+										? book.imageLinks.thumbnail
+										: `http://via.placeholder.com/128x193?text=No%20Cover`
+								}
+								title={book.title}
+								authors={book.authors}
+								shelf={book.shelf}
+								book={book}
+							/>
+						</li>
+					))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
+
 }
 
 export default Shelf
